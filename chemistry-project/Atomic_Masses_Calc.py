9c31d41794c3bceab60c5a89d51667229df91d85
Praketna <praketna142@gmail.com>
2026-09-21 22:11:09 -0400

    Add files via upload

---
+++ chemistry-project/Atomic_Masses_Calc.py
@@ -0,0 +1,19 @@
+#09/21/2026 - Calculating the total atomic mass from the periodic table CSV file
+
+ans = 0 #intialization of the variable to store the sum of atomic masses
+
+file = open("periodic_table.csv", "r") #Calling the file in read mode
+content = file.read() #Reading the content of the file
+lines = content.splitlines()
+
+
+for line in lines[1:]: #Skipping the header line
+    colums = line.split(",") #Every comma = a new column in the CSV file
+
+    if len(colums) > 3:
+        atomic_mass = float(colums[3].strip('""'))
+        ans = atomic_mass + ans
+
+print(f"Total Atomic Mass: {ans:.3f}")
+
+file.close() #Closing the file after reading
